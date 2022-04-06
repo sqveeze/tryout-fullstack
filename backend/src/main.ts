6ac458b7e463613b7ddaf97ed0e46dfa539d0be7
Márk Pap <sqveeze0@gmail.com>
2022-04-06 14:53:02 +0200

    :recycle: refactor bootstrap to use fastify instead of express

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
 }
-bootstrap();
+
+bootstrap().then(() =>
+  Logger.log('NestJS server started 🚀', 'NestApplication'),
+);
