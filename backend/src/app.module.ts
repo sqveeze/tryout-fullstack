7a447abadb7c6eb3ea9a3480ab544fb7b5f56565
Márk Pap <sqveeze0@gmail.com>
2022-04-06 15:45:47 +0200

    :wastebasket: remove unneeded files and code parts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { validationSchema } from './validations/config.validation';
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ validationSchema }), TransactionsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
