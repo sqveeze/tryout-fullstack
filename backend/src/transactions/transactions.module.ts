dc24d37674f13070a296054ca1ef3b29e5530ee2
Márk Pap <sqveeze0@gmail.com>
2022-04-06 16:20:33 +0200

    :recycle: try out the exchange service if its works as desired

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
 
 @Module({
+  imports: [ExchangeRateModule],
   controllers: [TransactionsController],
-  providers: [TransactionsService]
+  providers: [TransactionsService],
 })
 export class TransactionsModule {}
--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CalculateCommissionDto } from './dto/calculate-commission.dto';
+import { ExchangeRateService } from '../exchange-rate/exchange-rate.service';
 
 @Injectable()
 export class TransactionsService {
+  public constructor(
+    private readonly exchangeRateService: ExchangeRateService,
+  ) {}
+
   public async calculateCommission(
     calculateCommissionDto: CalculateCommissionDto,
   ): Promise<number> {
+    const res = await this.exchangeRateService.getExchangeRate();
+
+    console.log(res);
+
     return calculateCommissionDto.amount * 0.01;
   }
 }
