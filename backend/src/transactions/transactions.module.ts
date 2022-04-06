fac0cbce8355a0b2eb644506588d38c27874daef
Márk Pap <sqveeze0@gmail.com>
2022-04-06 17:24:55 +0200

    :recycle: inject transaction entity in transaction module

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
 
 @Module({
-  imports: [ExchangeRateModule],
+  imports: [ExchangeRateModule, TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
