063efc671e831d81bd77b56c545842ba6a8e975a
Márk Pap <sqveeze0@gmail.com>
2022-04-06 17:24:30 +0200

    :sparkles: entity model for transactions

---
+++ backend/src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ECurrency } from '@types';
+
+@Entity({ name: 'transactions' })
+export class Transaction extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  public readonly id: number;
+
+  @Column()
+  public date: string;
+
+  @Column()
+  public amount: number;
+
+  @Column({
+    type: 'enum',
+    enum: ECurrency,
+    default: ECurrency.USD,
+  })
+  public currency: ECurrency;
+
+  // This should be a foreign key for the clients table in a real world application
+  @Column()
+  public client_id: number;
+}
