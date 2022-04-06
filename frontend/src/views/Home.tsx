04781f0c79b952db7f2b0d8a2200fc322bff9fb5
Márk Pap <sqveeze0@gmail.com>
2022-04-06 20:34:15 +0200

    :recycle: Modify home page

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,9 +1,21 @@
+import Head from "next/head";
 import React from "react";
+
+import { CommissionCalculator } from "../components/pages/home/CommissionCalculator";
 
 export const HomeView: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>TryOut - Home</title>
+      </Head>
+      <div className="container py-4">
+        <div className="row">
+          <div className="col-12">
+            <CommissionCalculator />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
