51d65fe2ef07ccccf13ff561ed3e4a4405ea2db5
Márk Pap <sqveeze0@gmail.com>
2022-04-06 15:44:54 +0200

    :wastebasket: realized that there will be no DB needed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,16 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './config/database.config';
 import { validationSchema } from './validations/config.validation';
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ validationSchema }),
-    TypeOrmModule.forRootAsync(databaseConfig),
-    TransactionsModule,
-  ],
+  imports: [ConfigModule.forRoot({ validationSchema }), TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
