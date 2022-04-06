4c6b72656e060c468a955f991a531bd002dc41f7
Márk Pap <sqveeze0@gmail.com>
2022-04-06 18:44:46 +0200

    :recycle: base structure refactor

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from 'next'
+import {HomeView} from "../views/Home";
+
+const Home: NextPage = (): JSX.Element => <HomeView />
+
+export default Home
---
+++ frontend/src/views/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HomeView: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
