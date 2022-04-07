971976788d7b9a1683d574429a27654d4f91f24a
Márk Pap <sqveeze0@gmail.com>
2022-04-07 10:40:04 +0200

    :white_check_mark: unit test

--- backend/src/exchange-rate/exchange-rate.service.spec.ts
+++ backend/src/exchange-rate/exchange-rate.service.spec.ts
@@ -1,4 +1,7 @@
+import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ECurrency, IExchangeRateResponse } from '@types';
+
 import { ExchangeRateService } from './exchange-rate.service';
 
 describe('ExchangeRateService', () => {
@@ -7,12 +10,35 @@ describe('ExchangeRateService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ExchangeRateService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     service = module.get<ExchangeRateService>(ExchangeRateService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return exchange result', async () => {
+    const result: IExchangeRateResponse = {
+      base: ECurrency.EUR,
+      historical: false,
+      motd: undefined,
+      success: false,
+      date: '2020-01-01',
+      rates: {
+        HUF: 333.7,
+        USD: 1.1,
+        EUR: 1,
+      },
+    };
+
+    jest
+      .spyOn(service, 'getExchangeRate')
+      .mockImplementation(async () => result);
+
+    expect(await service.getExchangeRate()).toBe(result);
   });
 });
