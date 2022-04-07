61530e445577d000f35e1c00bf9fd4fe49fc9e92
Márk Pap <sqveeze0@gmail.com>
2022-04-07 10:39:36 +0200

    :white_check_mark: e2e test

---
+++ backend/test/transactions.e2e-spec.ts
@@ -0,0 +1,119 @@
+import { VersioningType } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockType } from '@types';
+import { validationSchema } from '@validations';
+import { Repository } from 'typeorm';
+
+import { ExchangeRateModule } from '../src/exchange-rate/exchange-rate.module';
+import { Transaction } from '../src/transactions/entities/transaction.entity';
+import { TransactionsController } from '../src/transactions/transactions.controller';
+import { TransactionsService } from '../src/transactions/transactions.service';
+
+let app: NestFastifyApplication;
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+    find: jest.fn((entity) => [entity]),
+    save: jest.fn((entity) => entity),
+  }),
+);
+
+beforeAll(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    imports: [
+      ExchangeRateModule,
+      ConfigModule.forRoot({ validationSchema: validationSchema }),
+    ],
+    controllers: [TransactionsController],
+    providers: [
+      TransactionsService,
+      {
+        provide: getRepositoryToken(Transaction),
+        useFactory: repositoryMockFactory,
+      },
+    ],
+  }).compile();
+
+  app = module.createNestApplication<NestFastifyApplication>(
+    new FastifyAdapter(),
+  );
+
+  await app.setGlobalPrefix('api');
+  await app.enableVersioning({ type: VersioningType.URI });
+  await app.init();
+  await app.getHttpAdapter().getInstance().ready();
+});
+
+it(`/POST calculate-commission`, () => {
+  return app
+    .inject({
+      method: 'POST',
+      url: 'api/v1/transactions/calculate-commission',
+      payload: {
+        date: '2022-04-05',
+        amount: 500,
+        currency: 'EUR',
+        client_id: 1,
+      },
+    })
+    .then((result) => {
+      expect(result.statusCode).toEqual(201);
+      expect(JSON.parse(result.payload)).toEqual({
+        amount: 2.5,
+        currency: 'EUR',
+      });
+    });
+});
+
+it(`/POST calculate-commission min 0.5`, () => {
+  return app
+    .inject({
+      method: 'POST',
+      url: 'api/v1/transactions/calculate-commission',
+      payload: {
+        date: '2022-04-05',
+        amount: 10,
+        currency: 'EUR',
+        client_id: 5,
+      },
+    })
+    .then((result) => {
+      expect(result.statusCode).toEqual(201);
+      expect(JSON.parse(result.payload)).toEqual({
+        amount: 0.05,
+        currency: 'EUR',
+      });
+    });
+});
+
+it(`/POST calculate-commission discounted client`, () => {
+  return app
+    .inject({
+      method: 'POST',
+      url: 'api/v1/transactions/calculate-commission',
+      payload: {
+        date: '2022-04-05',
+        amount: 500,
+        currency: 'USD',
+        client_id: 2,
+      },
+    })
+    .then((result) => {
+      expect(result.statusCode).toEqual(201);
+      expect(JSON.parse(result.payload)).toEqual({
+        amount: 2.29,
+        currency: 'EUR',
+      });
+    });
+});
+
+afterAll(async () => {
+  await app.close();
+});
