f5d16362137685ff91c6516a71fc6461bcf5abae
Márk Pap <sqveeze0@gmail.com>
2022-04-07 10:39:28 +0200

    :white_check_mark: unit test

--- backend/src/transactions/transactions.controller.spec.ts
+++ backend/src/transactions/transactions.controller.spec.ts
@@ -1,14 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockType } from '@types';
+import { Repository } from 'typeorm';
+
+import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
+import { Transaction } from './entities/transaction.entity';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+    find: jest.fn((entity) => [entity]),
+    save: jest.fn((entity) => entity),
+  }),
+);
 
 describe('TransactionsController', () => {
   let controller: TransactionsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ExchangeRateModule],
       controllers: [TransactionsController],
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     controller = module.get<TransactionsController>(TransactionsController);
--- backend/src/transactions/transactions.service.spec.ts
+++ backend/src/transactions/transactions.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockType } from '@types';
+import { Repository } from 'typeorm';
+
+import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
+import { Transaction } from './entities/transaction.entity';
 import { TransactionsService } from './transactions.service';
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+    find: jest.fn((entity) => [entity]),
+    save: jest.fn((entity) => entity),
+  }),
+);
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      imports: [ExchangeRateModule],
+      providers: [
+        TransactionsService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
