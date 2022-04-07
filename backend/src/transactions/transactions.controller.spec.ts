12399f93b338e9d43f25d37f497e3265ae7085c3
Márk Pap <sqveeze0@gmail.com>
2022-04-07 14:53:19 +0200

    :recycle: clean up tests

--- backend/src/transactions/transactions.controller.spec.ts
+++ backend/src/transactions/transactions.controller.spec.ts
@@ -1,20 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { MockType } from '@types';
-import { Repository } from 'typeorm';
 
 import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
-import { Transaction } from './entities/transaction.entity';
+import { TransactionRepository } from './repositories/transaction.repository';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
-
-export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
-  () => ({
-    findOne: jest.fn((entity) => entity),
-    find: jest.fn((entity) => [entity]),
-    save: jest.fn((entity) => entity),
-  }),
-);
 
 describe('TransactionsController', () => {
   let controller: TransactionsController;
@@ -26,8 +15,8 @@ describe('TransactionsController', () => {
       providers: [
         TransactionsService,
         {
-          provide: getRepositoryToken(Transaction),
-          useFactory: repositoryMockFactory,
+          provide: 'TransactionRepository',
+          useValue: TransactionRepository,
         },
       ],
     }).compile();
--- backend/src/transactions/transactions.service.spec.ts
+++ backend/src/transactions/transactions.service.spec.ts
@@ -1,19 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { MockType } from '@types';
-import { Repository } from 'typeorm';
+import { TransactionRepository } from 'typeorm';
 
 import { ExchangeRateModule } from '../exchange-rate/exchange-rate.module';
-import { Transaction } from './entities/transaction.entity';
 import { TransactionsService } from './transactions.service';
-
-export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
-  () => ({
-    findOne: jest.fn((entity) => entity),
-    find: jest.fn((entity) => [entity]),
-    save: jest.fn((entity) => entity),
-  }),
-);
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
@@ -24,8 +13,8 @@ describe('TransactionsService', () => {
       providers: [
         TransactionsService,
         {
-          provide: getRepositoryToken(Transaction),
-          useFactory: repositoryMockFactory,
+          provide: 'TransactionRepository',
+          useValue: TransactionRepository,
         },
       ],
     }).compile();
